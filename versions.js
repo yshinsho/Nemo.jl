8f678d4c83a4ce1ebccadc1da31359b3ebc72db6
autodocs <autodocs>
2019-05-27 21:12:03 +0000

    build based on e7dbb48

--- versions.js
+++ versions.js
@@ -1,11 +1,13 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.14",
   "release-0.13",
   "release-0.12",
   "release-0.11",
   "release-0.10",
   "release-0.6",
+  "v0.14.0",
   "v0.13.5",
   "v0.13.3",
   "v0.13.2",
